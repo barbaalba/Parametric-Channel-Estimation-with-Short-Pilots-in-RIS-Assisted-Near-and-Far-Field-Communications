a0d9dc6c87e76666d365515d4dafc5ea78928102
Mehdino <mehdi.haghshenas93@yahoo.com>
2023-01-17 12:18:12 +0100

    Add function comment

--- nearFieldApproxChan.m
+++ nearFieldApproxChan.m
@@ -1,4 +1,11 @@
 function phase = nearFieldApproxChan(d_t,azimuth,elevation,U,lambda)
+% This function use the first order approximation of taylor series to
+% approximate the near field 
+% Input
+%   - d_t: is the distance for the reference element
+%   - U: is the relative distance of each element from the array's center
+% Output
+%   - phase: The phase of all array considering the near field approx
 U = -U;
 M = size(U,2);
 T = length(d_t);
