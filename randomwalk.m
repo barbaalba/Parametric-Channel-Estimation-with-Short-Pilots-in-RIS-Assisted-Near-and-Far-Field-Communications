50de0911dccc233cb6e64b6fc756b2a0b0ad5be6
Mehdino <mehdi.haghshenas93@yahoo.com>
2023-05-28 10:31:02 +0200

    Add breakfrequency as input

--- randomwalk.m
+++ randomwalk.m
@@ -1,6 +1,4 @@
-function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax,SPlim,d_bjo,RIS)
-% We break each second into many sub period 
-breakfreq = 5;
+function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax,SPlim,d_bjo,RIS,breakfreq)
 x_t = zeros(NumUE,RandomLength*breakfreq);
 y_t = zeros(NumUE,RandomLength*breakfreq);
 xinit = unifrnd(Xmax/4,Xmax/2,NumUE,1);% Initilaize the location of the user
