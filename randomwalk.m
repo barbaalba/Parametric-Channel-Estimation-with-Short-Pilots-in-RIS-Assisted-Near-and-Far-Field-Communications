9542b5cd9764c7d35e68af76bd70135e906c2be8
Mehdino <mehdi.haghshenas93@yahoo.com>
2023-07-31 13:33:33 +0200

    Fixed the initial location based on the distance

--- randomwalk.m
+++ randomwalk.m
@@ -1,14 +1,27 @@
-function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax,SPlim,d_bjo,RIS,breakfreq)
-x_t = zeros(NumUE,RandomLength*breakfreq);
-y_t = zeros(NumUE,RandomLength*breakfreq);
-xinit = unifrnd(Xmax/8,Xmax/4,NumUE,1);% Initilaize the location of the user
-yinit = unifrnd(-Ymax/2,Ymax/2,NumUE,1);% Initilaize the location of the user
-x_t(:,1:breakfreq) = xinit;
+function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax,SPlim,d_bjo,d_NF,RIS,breakfreq)
+% this function generate a user walking randomly inside a room 
+% Input: 
+%   RIS: location of the RIS which is [0 0 0]
+%   RandomLength: time in second
+% Output: 
+%   x_t,y_t: are the location of the user in time
+x_t = zeros(NumUE,(RandomLength+1)*breakfreq);
+y_t = zeros(NumUE,(RandomLength+1)*breakfreq);
+
+% Initilaize the location of the user
+rinit = d_NF;
+anginit = unifrnd(0,pi,1);
+xinit = rinit*sin(anginit); 
+yinit = rinit*cos(anginit); 
+
+% user is static for one second
+x_t(:,1:breakfreq) = xinit; 
 y_t(:,1:breakfreq) = yinit;
+
 for m=1:NumUE
-  for n = 2:RandomLength % Looping all values of N into x_t(n).
-      SP = unifrnd(SPlim(1),SPlim(2)); % The movement speed
-      dir = unifrnd(-pi,pi); % The movement direction
+  for n = 2:RandomLength+1 % Looping all values of N into x_t(n).
+      SP = unifrnd(SPlim(1),SPlim(2)); % The movement speed for one second
+      dir = unifrnd(-pi,pi); % The movement direction for one second
       Xchange = SP/breakfreq*sin(dir);
       Ychange = SP/breakfreq*cos(dir);
       for l = 1:breakfreq
@@ -19,16 +32,17 @@ for m=1:NumUE
               x_t(m,breakfreq*(n-1)+l) = Xnew;
               y_t(m,breakfreq*(n-1)+l) = Ynew;
           else
-              x_t(m,breakfreq*(n-1)+l) = x_t(m,breakfreq*(n-1)+(l-1))-Xchange;
-              y_t(m,breakfreq*(n-1)+l) = y_t(m,breakfreq*(n-1)+(l-1))-Ychange;
+              Xchange = -Xchange; Ychange = -Ychange;
+              x_t(m,breakfreq*(n-1)+l) = x_t(m,breakfreq*(n-1)+(l-1))+Xchange;
+              y_t(m,breakfreq*(n-1)+l) = y_t(m,breakfreq*(n-1)+(l-1))+Ychange;
           end
       end
 
   end
 end
 
-x_t = x_t(breakfreq:end);
-y_t = y_t(breakfreq:end);
+x_t = x_t(breakfreq+1:end);
+y_t = y_t(breakfreq+1:end);
 
 end
 
