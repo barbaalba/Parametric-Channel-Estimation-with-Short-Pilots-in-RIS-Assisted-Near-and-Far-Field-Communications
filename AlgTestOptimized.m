2afad0ab8df1a37642ae311de37b7bd2744bb510
Mehdino <mehdi.haghshenas93@yahoo.com>
2023-12-23 14:36:34 +0100

    Add NMSE

--- AlgTestOptimized.m
+++ AlgTestOptimized.m
@@ -5,10 +5,11 @@ clc;clear;close all;
 freq = 28e9; % Central frequency
 lambda = physconst('LightSpeed') / freq; % Wavelength
 NFConf = false; % True or false to specify which case to simulate (Near field or far field)
+porpose = false; % True means it will estimate the channel through the proposed method
 LSConf = false; % To include the LS estimation of the channel
-Far_approx = false;
-Racian = true; % The Rician channel model 
-K = db2pow(10); % Rician K-factor in dB
+Far_approx = true;
+Racian = false; % The Rician channel model 
+K = db2pow(8); % Rician K-factor in dB
 
 %UPA Element configuration
 M_H = 32; M_V = 32; M = M_H*M_V;
@@ -38,16 +39,16 @@ R = UPAcorrelation(M_H,M_V,d_H,d_V,lambda); % correlation matrix
 
 %% Channel Estimation Parameters
 % search resolution (It is very important)
-varphiSRes = M_H;
-thetaSRes = M_V;
+varphiSRes = 4*M_H;
+thetaSRes = 2*M_V;
 distSRes = M_H; % Distance resolution is very important to avoid convergence
 Plim = 20; % number of pilots
 
 %Set the SNR
-SNRdB_pilot = 0;
+SNRdB_pilot = -10;
 SNR_pilot = db2pow(SNRdB_pilot);
 
-SNRdB_data = -10;
+SNRdB_data = -20;
 SNR_data = db2pow(SNRdB_data);
 
 %Select angle to the base station (known value)
@@ -59,8 +60,8 @@ h = UPA_Evaluate(lambda,M_V,M_H,varphi_BS,theta_BS,d_V,d_H);
 Dh = diag(h);
 Dh_angles = diag(h./abs(h));
 
-nbrOfAngleRealizations = 150;
-nbrOfNoiseRealizations = 4;
+nbrOfAngleRealizations = 40;
+nbrOfNoiseRealizations = 5;
 
 %Save the rates achieved at different iterations of the algorithm
 capacity = zeros(1,nbrOfAngleRealizations);
@@ -73,6 +74,9 @@ d_NMSE_proposed = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
 g_NMSE_proposed = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
 Far_d_NMSE_proposed = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
 Far_g_NMSE_proposed = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
+d_NMSE_LS = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
+v_NMSE_LS = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
+g_NMSE_LS = zeros(Plim,nbrOfAngleRealizations,nbrOfNoiseRealizations);
 %% Iniitilize channel estimation 
 [ElAngles,AzAngles,CBL] = UPA_BasisElupnew(M_V,M_H,d_V,d_H,pi/2,0);
 beamresponses = UPA_Codebook(lambda,ElAngles,AzAngles,M_V,M_H,d_V,d_H);
@@ -95,30 +99,40 @@ for n1 = 1:nbrOfAngleRealizations
 
     % Define a fine grid of angle directions and distance to be used in
     % estimator
-    varphi_range = linspace(azimuth-pi/24,azimuth+pi/24,varphiSRes);
+    varphi_range = linspace(azimuth-pi/12,azimuth+pi/12,varphiSRes);
     theta_range = linspace(elevation-pi/24,elevation+pi/24,thetaSRes);
     dist_range = zeros(1,distSRes);
-    mind = max([d_NF,d_t-d_fraun/4]);
-    maxd = min([10*d_fraun,d_t+d_fraun/4]);
+    if NFConf
+        mind = max([d_bjo,d_t-d_bjo/8]);
+        maxd = min([d_fraun,d_t+d_fraun/8]);
+    else
+        mind = max([d_NF,d_t-d_fraun/4]);
+        maxd = min([10*d_fraun,d_t+d_fraun/4]);
+    end
     dist_range(:) = linspace(mind,maxd,distSRes);
-    % obtain the array response vectors for all azimuth-elevation-distance
-    % triplet using the exact expression
-    a_range = zeros(M,varphiSRes,thetaSRes,distSRes); % [M,Azimuth,Elevation,distance]
-    for l =1:length(dist_range) % for each distance
-        d_red = repelem(dist_range(l),1,varphiSRes);
-        parfor i = 1:length(theta_range) % for each elevation
-            a_range(:,:,i,l) = ...
-                nearFieldChan(d_red,varphi_range,repelem(theta_range(i),1,varphiSRes),U,lambda);
+
+    if porpose
+        % obtain the array response vectors for all azimuth-elevation-distance
+        % triplet using the exact expression
+        a_range = zeros(M,varphiSRes,thetaSRes,distSRes); % [M,Azimuth,Elevation,distance]
+        for l =1:length(dist_range) % for each distance
+            d_red = repelem(dist_range(l),1,varphiSRes);
+            parfor i = 1:length(theta_range) % for each elevation
+                a_range(:,:,i,l) = ...
+                    nearFieldChan(d_red,varphi_range,repelem(theta_range(i),1,varphiSRes),U,lambda);
+            end
         end
     end
 
-    % Define a fine grid of angle directions to analyze when searching for
-    % angle of arrival in far field
-    a_FarAppx_range = zeros(M,varphiSRes,thetaSRes); % [M,Azimuth,Elevation]
-    % obtain the array response vectors for all azimuth-elevation pairs
-    parfor i = 1:thetaSRes
-    a_FarAppx_range(:,:,i) = ...
-    UPA_Evaluate(lambda,M_V,M_H,varphi_range,repelem(theta_range(i),1,varphiSRes),d_V,d_H);
+    if Far_approx
+        % Define a fine grid of angle directions to analyze when searching for
+        % angle of arrival in far field
+        a_FarAppx_range = zeros(M,varphiSRes,thetaSRes); % [M,Azimuth,Elevation]
+        % obtain the array response vectors for all azimuth-elevation pairs
+        parfor i = 1:thetaSRes
+        a_FarAppx_range(:,:,i) = ...
+        UPA_Evaluate(lambda,M_V,M_H,varphi_range,repelem(theta_range(i),1,varphiSRes),d_V,d_H);
+        end
     end
     
     if ~Racian     
@@ -137,59 +151,60 @@ for n1 = 1:nbrOfAngleRealizations
 
     for n2 = 1:nbrOfNoiseRealizations
         disp(n2);
-        % Estimate the channel using either all pilots or some of them
-        %Select which two RIS configurations from the grid of beams to start with
-        utilize = false(CBL+2,1);
-        utilize(end-1:end) = true;
-        
-        RISconfigs = Dh_angles*beamresponses(:,utilize);
-        B = RISconfigs';
-
         %Generate the noise
         noise = (randn(M+1,1)+1i*randn(M+1,1))/sqrt(2);
-
-         %Go through iterations by adding extra RIS configurations in the estimation
-        for itr = 1:Plim-1
-
-            %Generate the received signal
-            y =  sqrt(SNR_pilot)*(B*Dh*g + d) + noise(1:itr+1,1);
+        if porpose
+            % Estimate the channel using either all pilots or some of them
+            %Select which two RIS configurations from the grid of beams to start with
+            utilize = false(CBL+2,1);
+            utilize(end-1:end) = true;
             
-            % Estimate the Channel using the developed MLE
-            [d_est,var_phas_d_est,~,~,g_est,~,~] = MLE3D(y,itr+1,B,Dh,a_range,lambda,M_V,M_H,d_V,d_H,varphi_range,theta_range,SNR_pilot);
-            
-            % Estimation performance
-            d_NMSE_proposed(itr,n1,n2) = norm(d_est - d)^2 / norm(d)^2;
-            g_NMSE_proposed(itr,n1,n2) = norm(g_est - g)^2 / norm(g)^2;
-
-            %Estimate the RIS configuration that (approximately) maximizes the SNR
-            RISconfig = angle(Dh*g_est)-var_phas_d_est;
-
-            %Compute the corresponding achievable rate
-            rate_proposed(itr,n1,n2) = log2(1+SNR_data*abs(exp(-1i*RISconfig).'*Dh*g + d)^2);
-     
-            %Find an extra RIS configuration to use for pilot transmission
-            if itr < Plim-1 
-
-                %Find which angles in the grid-of-beams haven't been used
-                unusedBeamresponses = beamresponses;
-                unusedBeamresponses(:,utilize==1) = 0;
-                %Guess what the channel would be with the different beams
-                guessBeam = Dh*unusedBeamresponses;
-
-                %Find which of the guessed channels matches best with the
-                %currently best RIS configuration
-                closestBeam = abs(exp(-1i*RISconfig).'*guessBeam); 
-                [~,bestUnusedBeamidx] = max(closestBeam);
-
-
-                %Add a pilot transmission using the new RIS configuration
-                utilize(bestUnusedBeamidx) = true;
-                RISconfigs = Dh_angles*beamresponses(:,utilize);
-                B = RISconfigs';
-
+            RISconfigs = Dh_angles*beamresponses(:,utilize);
+            B = RISconfigs';
+    
+            %Go through iterations by adding extra RIS configurations in the estimation
+            for itr = 1:Plim-1
+    
+                %Generate the received signal
+                y =  sqrt(SNR_pilot)*(B*Dh*g + d) + noise(1:itr+1,1);
+                
+                % Estimate the Channel using the developed MLE
+                [d_est,var_phas_d_est,~,~,g_est,~,~] = MLE3D(y,itr+1,B,Dh,a_range,lambda,M_V,M_H,d_V,d_H,varphi_range,theta_range,SNR_pilot);
+                
+                % Estimation performance
+                d_NMSE_proposed(itr,n1,n2) = norm(d_est - d)^2 / norm(d)^2;
+                g_NMSE_proposed(itr,n1,n2) = norm(g_est - g)^2 / norm(g)^2;
+    
+                %Estimate the RIS configuration that (approximately) maximizes the SNR
+                RISconfig = angle(Dh*g_est)-var_phas_d_est;
+    
+                %Compute the corresponding achievable rate
+                rate_proposed(itr,n1,n2) = log2(1+SNR_data*abs(exp(-1i*RISconfig).'*Dh*g + d)^2);
+         
+                %Find an extra RIS configuration to use for pilot transmission
+                if itr < Plim-1 
+    
+                    %Find which angles in the grid-of-beams haven't been used
+                    unusedBeamresponses = beamresponses;
+                    unusedBeamresponses(:,utilize==1) = 0;
+                    %Guess what the channel would be with the different beams
+                    guessBeam = Dh*unusedBeamresponses;
+    
+                    %Find which of the guessed channels matches best with the
+                    %currently best RIS configuration
+                    closestBeam = abs(exp(-1i*RISconfig).'*guessBeam); 
+                    [~,bestUnusedBeamidx] = max(closestBeam);
+    
+    
+                    %Add a pilot transmission using the new RIS configuration
+                    utilize(bestUnusedBeamidx) = true;
+                    RISconfigs = Dh_angles*beamresponses(:,utilize);
+                    B = RISconfigs';
+    
+                end
             end
+            SNR_generic(:,n1,n2) = (abs(sqrt(SNR_pilot)*(B*Dh*g + d)).^2) ./ abs(noise(1:itr+1,1)).^2;
         end
-        SNR_generic(:,n1,n2) = (abs(sqrt(SNR_pilot)*(B*Dh*g + d)).^2) ./ abs(noise(1:itr+1,1)).^2;
 
         %% Far-Field approximation of the channel
         if Far_approx
@@ -206,7 +221,7 @@ for n1 = 1:nbrOfAngleRealizations
                 y =  sqrt(SNR_pilot)*(B*Dh*g + d) + noise(1:itr+1,1);
                 
                 % Estimate the Channel using the developed MLE
-                [d_est,var_phas_d_est,~,~,g_est,~,~] = MLE(y,itr+1,B,Dh,a_FarAppx_range,lambda,M_V,M_H,d_V,d_H,varphi_range,theta_range,SNR_pilot);
+                [d_est,var_phas_d_est,~,~,g_est,~,~] = MLE(y,itr+1,B,Dh,a_FarAppx_range,SNR_pilot);
                 
                 % Estimation performance
                 Far_d_NMSE_proposed(itr,n1,n2) = norm(d_est - d)^2 / norm(d)^2;
@@ -247,16 +262,19 @@ for n1 = 1:nbrOfAngleRealizations
             randomOrdering = randperm(M+1);
         
             %Go through iterations by adding extra RIS configurations in the estimation
-            for itr = 1:Plim-1
+            parfor itr = 1:Plim-1
 
                 B_LS = transpose(DFT(:,randomOrdering(1:itr+1)));
     
                 v = [d;Dh*g]; % cascaded channel and direct path 
                 y = sqrt(SNR_pilot)*B_LS*v + noise(1:itr+1,1);
-                 
+                
                 %Compute LS estimate without parametrization
                 v_LS = pinv(B_LS)*y/sqrt(SNR_pilot);
-    
+                % Estimation performance
+                d_NMSE_LS(itr,n1,n2) = norm(v_LS(1) - d)^2 / norm(d)^2;
+                v_NMSE_LS(itr,n1,n2) = norm(v_LS(2:end) - v(2:end))^2 / norm(v(2:end))^2;
+                g_NMSE_LS(itr,n1,n2) = norm(inv(Dh)*v_LS(2:end) - g)^2/norm(g)^2
                 %Estimate the RIS configuration that (approximately) maximizes the SNR
                 RISconfig =  angle(v_LS(2:end)) - angle(v_LS(1));
     
@@ -276,6 +294,7 @@ hold on;
 plot(log2(2:Plim),rate(1:Plim-1),'LineWidth',2);
 plot(log2(2:Plim),rate_FF(1:Plim-1),'LineWidth',2);
 plot(log2(2:Plim),rate_LS(1:Plim-1),'LineWidth',2);
+xlim([0 log2(Plim)]);
 xlabel('Number of pilots','FontSize',20,'Interpreter','latex');
 ylabel('Spectral Efficiency [b/s/Hz/]','FontSize',20,'Interpreter','latex');
 fig = gcf;
@@ -288,9 +307,10 @@ ax.XLabel.Units = 'normalized'; % Normalized unit instead of 'Data' unit
 ax.Position = [0.15 0.15 0.8 0.8]; % Set the position of inner axis with respect to
                            % the figure border
 ax.XLabel.Position = [0.5 -0.07]; % position of the label with respect to 
-                                  % axis
+xticklabels({'$0$','$2^2$','$2^4$','$2^6$','$2^8$','$2^{10}$'});                                  % axis
 fig = gcf;
 set(fig,'position',[60 50 900 600]);
+
 
 figure;
 d_NMSE = mean(mean(d_NMSE_proposed,3),2);
@@ -298,6 +318,8 @@ g_NMSE = mean(mean(g_NMSE_proposed,3),2);
 plot(2:Plim,pow2db(d_NMSE(1:Plim-1)),'LineWidth',2);
 hold on;
 plot(2:Plim,pow2db(g_NMSE(1:Plim-1)),'LineWidth',2);
+plot(2:Plim,pow2db(mean(mean(d_NMSE_LS(1:Plim-1,:,:),3),2)));
+plot(2:Plim,pow2db(mean(mean(g_NMSE_LS(1:Plim-1,:,:),3),2)));
 ylabel('NMSE [dB]','FontSize',20,'Interpreter','latex');
 xlabel('Number of pilots','FontSize',20,'Interpreter','latex');
 fig = gcf;
@@ -313,6 +335,6 @@ ax.XLabel.Position = [0.5 -0.07]; % position of the label with respect to
 fig = gcf;
 set(fig,'position',[60 50 900 600]);
 
-save('FinalSISORician.mat','rate_proposed','rate_LS',...
-     'Far_rate_proposed','capacity','Plim','SNR_generic','SNR_far',...
-     'd_NMSE_proposed','g_NMSE_proposed');
+save('FinalSISOLOS_LowSNR_NearFeild_together_new.mat','rate_proposed','rate_LS',...
+      'Far_rate_proposed','capacity','Plim','SNR_generic','SNR_far',...
+      'd_NMSE_proposed','g_NMSE_proposed','g_NMSE_LS','d_NMSE_LS');
