360f7d8531904df2d3f554310a1ffb92ba879f08
Mehdino <mehdi.haghshenas93@yahoo.com>
2024-02-05 00:11:14 +0330

    Update Hcwgenerate.m

--- Hcwgenerate.m
+++ Hcwgenerate.m
@@ -6,27 +6,35 @@ function W = Hcwgenerate(N,d,lambda)
 
 K = log2(N); % number of layers in hierarchical design
 W = struct;
-for i = 0:1:K
-    eval(['W.Layer' num2str(i) '= [];']);
+rl = {'_right','_left'};
+eval(['W.Layer' num2str(0)  '= [];']);
+for i = 1:1:K-1
+    for r = 1:2
+        eval(['W.Layer' num2str(i) rl{r} '= [];']);
+    end
 end
+eval(['W.Layer' num2str(K)  '= [];']);
 fl = fieldnames(W);
 % The last layer which has sharpest beam
 n = 1:N; azimuth = asin(-1+(2*n-1)/N);
 W.(fl{end}) = ULA_Evaluate(lambda,N,azimuth,d);
-
-for l = 1:K
-    k = K-l;
-    M = 2^floor((l+1)/2); % subarrays
-    N_s = N/M; % number of subarray elements
-    N_a = (1-mod(l,2))*M + mod(l,2)*M/2; % subarray activation
-    m = 1:N_a; azimuth = asin(-1+(2*m-1)/N_s);
-    f_m = exp(-1i*m*(N_s-1)/N_s*pi).*ULA_Evaluate(lambda,N_s,azimuth,d);
-    % The first beam in each layer
-    w = zeros(N,2^k); 
-    w(1:N_a*N_s,1) = reshape(f_m,N_s*N_a,1);
-    n = 2:2^k; azimuth = asin(2*(n-1)/N);
-    w(:,2:end) = w(:,1) .* ULA_Evaluate(lambda,N,azimuth,d);
-    W.(fl{k+1}) = w;
+% make the code book for lower layer
+for l = 1:K-1
+    for r = 1:2
+        k = K-l; % Which layer
+        M = 2^floor((l+1)/2); % subarrays
+        N_s = N/M; % number of subarray elements
+        N_a = (1-mod(l,2))*M + mod(l,2)*M/2; % subarray activation
+        m = 1:N_a; azimuth = (-1)^(r+1)*asin(-1+(2*m-1)/N_s);
+        f_m = exp(-1i*m*(N_s-1)/N_s*pi).*ULA_Evaluate(lambda,N_s,azimuth,d);
+        % The first beam in each layer
+        w = zeros(N,2^k); 
+        w(1:N_a*N_s,1) = reshape(f_m,N_s*N_a,1);
+        % The other beams in the layer
+        n = 2:2^k; azimuth = (-1)^(r+1)*asin(2*(n-1)/N);
+        w(:,2:end) = w(:,1) .* ULA_Evaluate(lambda,N,azimuth,d);
+        W.(fl{end-(2*(l-1)+r)}) = w;
+    end
 end
 
 end
