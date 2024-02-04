6e462be15569aca202a0f83067afc8b0f768086a
Mehdino <mehdi.haghshenas93@yahoo.com>
2024-02-05 00:02:15 +0330

    Fixed a bug in the code

--- altopt.m
+++ altopt.m
@@ -6,18 +6,21 @@ function RIS_opt = altopt(H,g,d)
 %   - g: chanel betweeen RIS and UE
 %   - d: direct channel between BS and UE
 epoch = 20;
-V = H*diag(g);  % cascaded channel
-Vbar = V' * V;
+G = H*diag(g);  % cascaded channel
+Gbar = G' * G;
 N = length(g);
 theta = ones(N,1);
 rate = zeros(1,epoch);
+
 % How many epoch
 for j = 1:epoch
     % optimize over i-th RIS phase shift
     for i = 1:N
-        theta(i) = exp(1i * angle(theta.'* Vbar(i,:).' + d.' * V(:,i)));
+        idx = 1:N;
+        idx(i) = [];
+        theta(i) = exp(-1i * angle(theta(idx)'* Gbar(idx,i) + d' * G(:,i)));
     end
-    rate(j) = log2(1+(norm(V*theta + d)^2));
+    rate(j) = log2(1+(norm(G*theta + d)^2));
 end
 RIS_opt = theta;
 plot(1:epoch,rate);
