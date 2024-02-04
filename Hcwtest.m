15eea21bc5814caa2f349ac4369212119602665f
Mehdino <mehdi.haghshenas93@yahoo.com>
2024-02-05 00:13:11 +0330

    Update Hcwtest.m

--- Hcwtest.m
+++ Hcwtest.m
@@ -5,9 +5,9 @@ M = 32; d = 1/2;
 W = Hcwgenerate(M,d,lambda);
 fl = fieldnames(W);
 resphi = -pi/2:pi/360:pi/2;
-for k = 1:length(fl)
+for k = 2:length(fl)
     w = W.(fl{k});
-    gain =  abs(1/sqrt(M)*w'*ULA_Evaluate(lambda,M,resphi,d)).^2;
+    gain =  abs(1/sqrt(M)*w'*ULA_Evaluate(M,resphi,d)).^2;
     figure;
     ax = polaraxes;
     polarplot(resphi+pi/2,pow2db(gain),'LineWidth',2); 
@@ -16,9 +16,10 @@ for k = 1:length(fl)
     set(ax, 'ThetaTick', [0, 30, 60, 90, 120, 150, 180]);
     set(ax, 'TickLabelInterpreter', 'latex');
     set(ax, 'ThetaTickLabel',{'$-\frac{\pi}{2}$','$-\frac{\pi}{3}$','$-\frac{\pi}{6}$','$0$','$\frac{\pi}{6}$','$\frac{\pi}{3}$','$\frac{\pi}{2}$'});
-    set(ax,'RLim',[-5 pow2db(M)]);
+    set(ax,'RLim',[-5 pow2db(32)]);
     set(gca,'fontsize',18);
     ax.ThetaLim = [0 180];
+    title(fl{k});
 end
 
 
