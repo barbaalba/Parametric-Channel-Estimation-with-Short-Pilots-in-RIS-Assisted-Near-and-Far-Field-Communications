e5b04a2a387f6696c6752a46552bb86440e90828
Mehdino <mehdi.haghshenas93@yahoo.com>
2024-03-30 12:22:32 +0100

    Add function comment

--- plotTrajectory.m
+++ plotTrajectory.m
@@ -1,6 +1,16 @@
 function plotTrajectory(x_t,y_t,Az,El,conf,Xmax,Ymax,RIS_coor,d_bjo,d_NF)
-K = size(x_t,1);
-T = size(x_t,2);
+%       RIS_Loc = [0,0,0]
+%   ----------RIS---------- x = 0
+%   |                      |
+%   |                      |
+%   |        ROOM          | 
+%   |                      |
+%   |                      |
+%   ----------------------- x = Xmax
+% -Ymax                   Ymax  
+%
+K = size(x_t,1); % number of user
+T = size(x_t,2); % Time length
 thet = 0:pi/180:2*pi;
 xbjo = d_bjo*cos(thet) + RIS_coor(2);
 ybjo = d_bjo*sin(thet) + RIS_coor(1);
@@ -11,14 +21,15 @@ for k = 1:K
     fig = gcf;
     set(fig,'position',[60 50 1600 800]); % [left bottom width height]
     %tiledlayout(2,1);
-    subplot(2,2,1);set(gca, 'ydir', 'reverse');xlabel('y');ylabel('x');
-    grid on; xlim([-Ymax,Ymax]); ylim([-Xmax,Xmax]); hold on;
-    plot(RIS_coor(2),RIS_coor(1),'Marker','square','MarkerSize',10,'Color','r');
-    subplot(2,2,2);xlabel('Time (s)','Interpreter','latex','FontSize',20);
-    ylabel('Azimuth','Interpreter','latex','FontSize',20);grid on;ylim([-90,90]);
+    subplot(2,2,1);set(gca, 'ydir', 'reverse');
+    xlabel('y','FontSize',20,'Interpreter','latex');ylabel('x','FontSize',20,'Interpreter','latex');
+    grid on; xlim([-Ymax,Ymax]); ylim([0,Xmax]); hold on;
+    plot(RIS_coor(2),RIS_coor(1),'Marker','square','MarkerSize',10,'Color','r'); % plot RIS in the room
+    subplot(2,2,2);xlabel('Time (ms)','Interpreter','latex','FontSize',20);
+    ylabel('Azimuth [degree]','Interpreter','latex','FontSize',20);grid on;ylim([-90,90]);
     xlim([1,T]);hold on;yticks([-90 -45 0 45 90]);
     subplot(2,2,[3,4]);xlabel('Time (s)','Interpreter','latex','FontSize',20);
-    ylabel('Elevation','Interpreter','latex','FontSize',20); grid on;
+    ylabel('Elevation [degree]','Interpreter','latex','FontSize',20); grid on;
     ylim([-90,0]);xlim([1,T]); hold on;
     for t = 2:T
     %nexttile(1);
@@ -28,7 +39,11 @@ for k = 1:K
         plot(RIS_coor(2),RIS_coor(1),'Marker','square','MarkerSize',10,'Color','r'); % RIS Location
         hold on;
         % The user movement
-        plot(y_t(k,t-1:t),x_t(k,t-1:t),'Color','b'); set(gca, 'ydir', 'reverse');xlabel('y');ylabel('x');xlim([-Xmax,Xmax]); ylim([-Ymax,Ymax]);grid on;
+        plot(y_t(k,t-1:t),x_t(k,t-1:t),'Color','b','MarkerSize',20); 
+        set(gca, 'ydir', 'reverse');
+        xlabel('y','FontSize',20,'Interpreter','latex');
+        ylabel('x','FontSize',20,'Interpreter','latex');
+        xlim([-Ymax,Ymax]); ylim([0,Xmax]);grid on;
         plot(xbjo,ybjo,'--k','LineWidth',4);
         plot(xNF,yNF,'--k','LineWidth',4);
         hold off;
@@ -41,9 +56,9 @@ for k = 1:K
     %nexttile(2);
     subplot(2,2,2);
     plot(rad2deg(Az(k,1:t)),'r','Marker','*','MarkerSize',4);
-    pause(0.01);
-    subplot(2,2,[3,4]);
-    plot(rad2deg(El(k,1:t)),'k','Marker','*','MarkerSize',4);
-    pause(0.01);
+    pause(0.00001);
+%     subplot(2,2,[3,4]);
+%     plot(rad2deg(El(k,1:t)),'k','Marker','*','MarkerSize',4);
+%     pause(0.00001);
     end
 end
